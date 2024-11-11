9b66c323c6ba1f81cf236e3872dae51bf3a915bd
Damian <davarayud@gmail.com>
2024-11-11 03:31:46 -0300

    agrego configuraciones por defecto por si no se crea el .env

--- fifa-api/src/utils/config.js
+++ fifa-api/src/utils/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
-const BASE_DE_DATOS = process.env.BASE_DE_DATOS
-const USUARIO_DB = process.env.USUARIO_DB
-const PASSWORD_USUARIO_DB = process.env.PASSWORD_USUARIO_DB
-const HOST = process.env.HOST
-const SECRET_JWT = process.env.SECRET_JWT
+const PORT = process.env.PORT || 3003
+const BASE_DE_DATOS = process.env.BASE_DE_DATOS || 'fifa_local'
+const USUARIO_DB = process.env.USUARIO_DB || 'root'
+const PASSWORD_USUARIO_DB = process.env.PASSWORD_USUARIO_DB || 'root'
+const HOST = process.env.HOST || 'localhost'
+const SECRET_JWT = process.env.SECRET_JWT || 'Palabra secreta'
 
 module.exports = {
   PORT,
